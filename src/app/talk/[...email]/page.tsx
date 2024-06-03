d8768ebc47e79dfab7f8cf65200f8bb8064db22b
lmmagalhaes <leo.maga17@gmail.com>
2024-06-03 08:09:48 -0300

    add confetti to /talk

--- src/app/talk/[...email]/page.tsx
+++ src/app/talk/[...email]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import confetti from 'canvas-confetti'
 import axios from 'axios'
 
 export default function ShowUser({ params }: any) {
@@ -10,6 +11,36 @@ export default function ShowUser({ params }: any) {
     cpf: '',
   })
   const { email } = params
+  const duration = 15 * 1000
+  const animationEnd = Date.now() + duration
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 }
+
+  function randomInRange(min: number, max: number) {
+    return Math.random() * (max - min) + min
+  }
+
+  const interval: any = () => {
+    setInterval(function () {
+      var timeLeft = animationEnd - Date.now()
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval)
+      }
+
+      var particleCount = 50 * (timeLeft / duration)
+      // since particles fall down, start a bit higher than random
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      })
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      })
+    }, 250)
+  }
   useEffect(() => {
     if (!email) {
       return
@@ -17,6 +48,7 @@ export default function ShowUser({ params }: any) {
     axios.get(`/api/talk?email=${decodeURIComponent(email)}`).then((res) => {
       setUserInfo(res.data)
     })
+    interval()
   }, [email])
   return (
     <div className="h-screen flex justify-center items-center bg-slate-600 px-5">
